b08c8cb80c8c27297b3cc9505fe4655911b0a469
David Jones <coreldavid@hotmail.com>
2020-09-17 22:06:17 -0400

    Allow user to decide number of rolls

--- src/com/DavidJones/Main.java
+++ src/com/DavidJones/Main.java
@@ -1,17 +1,26 @@
 package com.DavidJones;
 
 import GameDice.Die;
+
+import java.util.Scanner;
+
 import static java.lang.System.out;
 
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        //variables
         Die myDie = new Die(6);
-        out.println(myDie.getSides());
+        int numberOfRolls = 0;
+        Scanner input = new Scanner(System.in);
+
+        //out.println(myDie.getSides());
+        out.println("Enter number of rolls");
+        numberOfRolls = input.nextInt();
 
         //test 3 rolls
-        for(int rolls = 0; rolls < 20; rolls++){
+        for(int rolls = 0; rolls < numberOfRolls; rolls++){
             out.println(myDie.dieRoll());
         }
     }
