1de96ab9f2e0c1cf7dc3551a29c712a7f5351cfd
David Jones <coreldavid@hotmail.com>
2020-09-17 21:52:16 -0400

    Die Random Roll

--- src/GameDice/Die.java
+++ src/GameDice/Die.java
@@ -5,12 +5,21 @@ public class Die {
     private int dieSides;
     private Random randomNumber;
 
-    public void die(){
+    public Die(){
         this.dieSides = 4;
     }
 
-    public void die(int newAmountOfSides){
+    public Die(int newAmountOfSides){
         this.dieSides = newAmountOfSides;
     }
 
+    public int getSides(){
+        return dieSides;
+    }
+
+    public int dieRoll(){
+        randomNumber = new Random();
+        return randomNumber.nextInt((dieSides - 1) + 1) + 1;
+
+    }
 }
--- src/com/DavidJones/Main.java
+++ src/com/DavidJones/Main.java
@@ -1,11 +1,18 @@
 package com.DavidJones;
 
 import GameDice.Die;
+import static java.lang.System.out;
 
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        public Die myDie
+        Die myDie = new Die(6);
+        out.println(myDie.getSides());
+
+        //test 3 rolls
+        for(int rolls = 0; rolls < 20; rolls++){
+            out.println(myDie.dieRoll());
+        }
     }
 }
