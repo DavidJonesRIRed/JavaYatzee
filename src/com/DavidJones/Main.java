00ad0dae763efe41f937867d73c7a856a8033622
David Jones <coreldavid@hotmail.com>
2020-09-21 19:12:16 -0400

    roll multiple dice

--- src/com/DavidJones/Main.java
+++ src/com/DavidJones/Main.java
@@ -2,6 +2,7 @@ package com.DavidJones;
 
 import GameDice.Die;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 import static java.lang.System.out;
@@ -11,17 +12,35 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         //variables
-        Die myDie = new Die(6);
+        //Die myDie = new Die(6);
         int numberOfRolls = 0;
         Scanner input = new Scanner(System.in);
+        int numberOfDice = 0;
+        ArrayList<Die> myDice = new ArrayList<>();
+
+
+
+
+        out.println("Please enter the number of Dice");
+        numberOfDice = input.nextInt();
+
+        for(int makeDice = 0; makeDice < numberOfDice; makeDice++){
+            //String varName = "die" + Integer.toString(makeDice);
+            Die myDie = new Die();
+            myDice.add(myDie);
+        }
+
 
         //out.println(myDie.getSides());
         out.println("Enter number of rolls");
         numberOfRolls = input.nextInt();
 
-        //test 3 rolls
+        //test rolls
         for(int rolls = 0; rolls < numberOfRolls; rolls++){
-            out.println(myDie.dieRoll());
+            for(int dieNumber = 0; dieNumber < numberOfDice; dieNumber++){
+                out.println("This is die number " + dieNumber + " roll result " +myDice.get(dieNumber).dieRoll());
+            }
+
         }
     }
 }
